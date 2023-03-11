d837a1119cd9e05f3e2639c785b9ad3ef1f5cb19
Anushka Pawaskar <122295599+AnushkaPawaskar@users.noreply.github.com>
2023-03-11 18:59:50 +0530

    Assignment

---
+++ Assignment11Q3.js
@@ -0,0 +1,16 @@
+const strOne = "good development"; 
+const strTwo = "Hello all";
+ const strThree = "learn";
+  let newstrOne =strOne.replace("good","web");
+ //console.log(newstrOne);
+ let newstrTwo=strTwo.replace("all"," WEBx86_Students")
+ //console.log(newstrTwo);
+ let newstr=(`${newstrTwo} ${strThree} ${newstrOne}`);
+ console.log(newstr);
+
+
+
+
+
+
+ // “Hello WEBx86_Students learn web develop
